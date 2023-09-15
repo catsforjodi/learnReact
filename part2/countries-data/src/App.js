5d034b497923c7ba8908a11d77c47017226edbc9
jodi chan windows <jodihcc@gmail.com>
2023-09-15 15:49:57 -0700

    - printed out "list" if less than 10 countries and more than 1 - Prompt if more than 10 matches

--- part2/countries-data/src/App.js
+++ part2/countries-data/src/App.js
@@ -1,13 +1,14 @@
 // https://fullstackopen.com/en/part2/adding_styles_to_react_app#exercises-2-18-2-20
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import Prompt from './components/Prompt'
 const App = () => {
   // STATES
   const [value, setValue] = useState('')
   const [countryNames, setCountryNames] = useState([])
-  const [country, setCountry] = useState(null)
-  
-
+  const [country, setCountry] = useState([])  
+  const [countries, setCountries] = useState([])  
+  const [tooManyMatches, isTooManyMatches] = useState(false)
   // EFFECTS
 
   // maps ALL countries object into array of names
@@ -26,6 +27,15 @@ const App = () => {
     console.log('input value:', value)
     const matchedCountries = countryNames.filter((country) => country.toLowerCase().includes(value.toLowerCase()))
     console.log('matchedCountries', matchedCountries)
+    if (matchedCountries.length > 10) {
+      console.log('BEAR')
+      isTooManyMatches(true)
+    } else if (matchedCountries.length > 1 && matchedCountries.length <= 10) {
+      isTooManyMatches(false)
+      setCountries(matchedCountries)
+    } else if (matchedCountries.length === 1) {
+      setCountry(matchedCountries[0])
+    }
   }, [value])
 
   // useEffect(() => {
@@ -53,7 +63,11 @@ const App = () => {
           name="MyInput"
           onChange = {handleChange}
           />
+        <Prompt tooManyMatches={tooManyMatches}/>
+        {countries.map(country => <div>{country}</div>)}
+        
     </div>
+
   )
 }
 
---
+++ part2/countries-data/src/components/Prompt.js
@@ -0,0 +1,11 @@
+
+
+const Prompt = ({tooManyMatches}) => {
+    if (tooManyMatches) {
+        return <div>Too many matches, specify another filter</div>
+    }
+}
+
+export default Prompt
+
+
